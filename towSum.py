0885c88f86175ea010bc592081d5cfa63e890b61
linjunyi <ljyalan@126.com>
2018-04-11 23:49:44 +0800

    两数之和

---
+++ towSum.py
@@ -0,0 +1,26 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+给定一个整数数列，找出其中和为特定值的那两个数。
+你可以假设每个输入都只会有一种答案，同样的元素不能被重用。
+"""
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0,len(nums)):
+        	for j in range(i+1,len(nums)):
+        		if nums[i] + nums[j] == target:
+        			return [i,j]
+
+nums = [1,2,3,4,5,6]
+print(Solution().twoSum(nums,11))
