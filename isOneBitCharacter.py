68f1740d9a456375d56b06634db4cd4c79186822
linjunyi <ljyalan@126.com>
2018-07-18 22:48:12 +0800

    1比特与2比特字符

---
+++ isOneBitCharacter.py
@@ -0,0 +1,42 @@
+"""
+We have two special characters. The first character can be represented by one bit 0. The second character can be represented by two bits (10 or 11).
+Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. The given string will always end with a zero.
+
+有两种特殊字符。第一种字符可以用一比特0来表示。第二种字符可以用两比特(10 或 11)来表示。
+现给一个由若干比特组成的字符串。问最后一个字符是否必定为一个一比特字符。给定的字符串总是由0结束。
+
+示例 1:
+输入: 
+bits = [1, 0, 0]
+输出: True
+解释: 
+唯一的编码方式是一个两比特字符和一个一比特字符。所以最后一个字符是一比特字符。
+
+示例 2:
+输入: 
+bits = [1, 1, 1, 0]
+输出: False
+解释: 
+唯一的编码方式是两比特字符和两比特字符。所以最后一个字符不是一比特字符。
+"""
+
+class Solution(object):
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+        """
+        if len(bits) == 1:
+            return True
+
+        tmp = bits[:len(bits)-1]
+        for i in range(len(tmp)-1):
+            if tmp[i] == 1:
+                tmp[i],tmp[i+1] = 0,0
+        if tmp.count(1) == 1:
+            return False
+        else:
+            return True
+
+test = Solution().isOneBitCharacter([1,1,1,0])
+print(test)
