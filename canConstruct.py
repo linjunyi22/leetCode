d49ddde076570ef4da81715b35469a40634bef68
linjunyi <ljyalan@126.com>
2018-08-06 22:48:49 +0800

    赎金信

---
+++ canConstruct.py
@@ -0,0 +1,30 @@
+"""
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+Each letter in the magazine string can only be used once in your ransom note.
+
+给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串ransom能不能由第二个字符串magazines里面的字符构成。如果可以构成，返回 true ；否则返回 false。
+(题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，组成单词来表达意思。)
+
+注意：
+你可以假设两个字符串均只含有小写字母。
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+"""
+
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        for i in ransomNote:
+            if ransomNote.count(i) > magazine.count(i):
+                return False
+        return True
+
+test = Solution().canConstruct('aa','ab')
+print(test)
