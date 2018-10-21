14c18fb2c8d8fb800b44c6ef66c5601cf8904514
linjunyi <ljyalan@126.com>
2018-10-21 19:59:14 +0800

    496. 下一个更大元素 I

---
+++ nextGreaterElement.py
@@ -0,0 +1,43 @@
+"""
+You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+
+The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+
+给定两个没有重复元素的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出-1。
+
+示例 1:
+输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+输出: [-1,3,-1]
+解释:
+    对于num1中的数字4，你无法在第二个数组中找到下一个更大的数字，因此输出 -1。
+    对于num1中的数字1，第二个数组中数字1右边的下一个较大数字是 3。
+    对于num1中的数字2，第二个数组中没有下一个更大的数字，因此输出 -1。
+
+注意:
+1.nums1和nums2中所有元素是唯一的。
+2.nums1和nums2 的数组大小都不超过1000。
+
+"""
+
+
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        for i in nums1:
+            index = nums2.index(i)
+            if i == nums2[-1]:
+                result.append(-1)
+            elif max(nums2[index+1:]) < i:
+                result.append(-1)
+            else:
+                result.append([j for j in nums2[index+1:] if j > i][0])
+        return result
+
+test = Solution().nextGreaterElement([4,1,2],[1,3,4,2])
+print(test)
