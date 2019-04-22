dd1d28e737a12b4a935b648367a98f7ce86f5b2f
linjunyi <ljyalan@126.com>
2019-04-22 23:30:37 +0800

    205. 同构字符串

---
+++ isIsomorphic.py
@@ -0,0 +1,52 @@
+"""
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+给定两个字符串 s 和 t，判断它们是否是同构的。
+
+如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
+
+所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+
+示例 1:
+输入: s = "egg", t = "add"
+输出: true
+
+示例 2:
+输入: s = "foo", t = "bar"
+输出: false
+
+示例 3:
+输入: s = "paper", t = "title"
+输出: true
+
+说明:
+你可以假设 s 和 t 具有相同的长度。
+"""
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        d = {}
+        for i in range(len(s)):
+            if s[i] in d:
+                if d[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in d.values():
+                    return False
+                d[s[i]] = t[i]
+        return True
+
+if __name__ == '__main__':
+    s = 'egg'
+    t = 'add'
+    test = Solution().isIsomorphic(s, t)
+    print(test)
