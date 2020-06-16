d74574e8a604472a6fc5955c34ffe136fd5c231d
linjunyi <ljyalan@126.com>
2020-06-16 17:42:46 +0800

    814. 二叉树剪枝

---
+++ pruneTree.py
@@ -0,0 +1,33 @@
+"""
+给定二叉树根结点 root ，此外树的每个结点的值要么是 0，要么是 1。
+
+返回移除了所有不包含 1 的子树的原二叉树。
+
+( 节点 X 的子树为 X 本身，以及所有 X 的后代。)
+
+示例1:
+输入: [1,null,0,0,1]
+输出: [1,null,0,null,1]
+ 
+解释: 
+只有红色节点满足条件“所有不包含 1 的子树”。
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if root.val == 0 and not root.left and not root.right:
+            return None
+        return root
